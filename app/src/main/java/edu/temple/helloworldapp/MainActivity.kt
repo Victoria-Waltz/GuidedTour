116d3ed0b5d251d2fbceb04b7d334bb01b0e5019
Victoria Waltz <148576621+Victoria-Waltz@users.noreply.github.com>
2025-09-04 15:17:08 -0400

    Added "Click me!" button

--- app/src/main/java/edu/temple/helloworldapp/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworldapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package edu.temple.helloworldapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,7 +10,12 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState) // NEED (Do not touch)
+        setContentView(R.layout.activity_main) // NEED (Do not touch)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            findViewById<TextView>(R.id.textView).text = "You clicked the button!"
+        }
+
     }
 }
